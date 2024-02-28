8520931a6b66f45fea05f33dde9970cb3dfcae2f
Claudia <claudiabarcelovaldes40@gmail.com>
2024-02-28 16:59:31 +0100

    fix: test

--- test/Solution_2.t.sol
+++ test/Solution_2.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {Solution_2} from "../src/lesson2/Solution_2.sol";
 
 contract Solution_2_Test is Test {
@@ -16,10 +16,10 @@ contract Solution_2_Test is Test {
     }
 
     function test_DeleteItemByPos(uint256 pos_) public {
-        uint256 _length = items.length;
-        if (pos_ >= _length) {
+        uint256 _lengthBefore = solution.itemsCount();
+        if (pos_ >= _lengthBefore) {
             vm.expectRevert(
-                abi.encodeWithSelector(OutOfRange.selector, pos_, _length)
+                abi.encodeWithSelector(OutOfRange.selector, pos_, _lengthBefore)
             );
             solution.deleteItemByPos(pos_);
         } else {
@@ -27,11 +27,17 @@ contract Solution_2_Test is Test {
 
             solution.deleteItemByPos(pos_);
 
-            uint256 _itemAtPosAfter = solution.items(pos_);
+            // check length
+            assertEq(solution.itemsCount(), _lengthBefore - 1);
 
-            // check
-            assertEq(solution.itemsCount(), _length - 1);
-            assertNotEq(_itemAtPosBefore, _itemAtPosAfter);
+            // check previous item at pos_ was removed
+            if (pos_ == _lengthBefore - 1) {
+                vm.expectRevert();
+                solution.items(pos_);
+            } else {
+                uint256 _itemAtPosAfter = solution.items(pos_);
+                assertNotEq(_itemAtPosBefore, _itemAtPosAfter);
+            }
         }
     }
 
