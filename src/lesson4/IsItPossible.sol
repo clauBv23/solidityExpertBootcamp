453a8bd5c5ac1898d8ef4c13901118f5a2f006c6
Claudia <claudiabarcelovaldes40@gmail.com>
2024-02-29 18:18:29 +0100

    feat: solution to last task lesson 4

---
+++ src/lesson4/IsItPossible.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract MyCon {
+    function a(
+        function(
+            function(
+                function(
+                    function(
+                        function(
+                            function(
+                                function(
+                                    function(
+                                        function(
+                                            function(function() external pure)
+                                                external
+                                                pure
+                                        ) external pure
+                                    ) external pure
+                                ) external pure
+                            ) external pure
+                        ) external pure
+                    ) external pure
+                ) external pure
+            ) external pure
+        ) external pure
+    ) external pure {}
+}
